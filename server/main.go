72573a83de72788d7d2ee1ccb0949b81cb170a56
Samuel787 <suther.david.samuel@gmail.com>
2021-08-10 10:43:17 +0800

    [Test] CREATE tested

--- server/main.go
+++ server/main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
 	"./router"
 )
 
 func main() {
 	r := router.Router()
-	fmt.Println("Starting server on the port 8080...")
+	fmt.Println("Starting server on the port 27017...")
 	log.Fatal(http.ListenAndServe(":27017", r))
 }
